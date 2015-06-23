c78e2925b96298c385b8b4b30c9304e1e361fdfc
Tobias Brandt <Tobias.Brandt@gmail.com>
2015-06-23 10:22:22 +0200

    DEV: Added support for IPython 2.x in nbinstall(). Fixes #16.

--- qgrid/__init__.py
+++ qgrid/__init__.py
@@ -11,16 +11,28 @@ def nbinstall(user=True, overwrite=False):
     # Lazy imports so we don't pollute the namespace.
     import os
     from IPython.html.nbextensions import install_nbextension
+    from IPython import version_info
 
     qgridjs_path = os.path.join(
         os.path.dirname(__file__),
         'qgridjs',
     )
 
-    install_nbextension(
-        qgridjs_path,
-        overwrite=overwrite,
-        user=user,
-        symlink=False,
-        verbose=0,
-    )
+    if version_info >= (3, 0, 0, ''):
+        install_nbextension(
+            qgridjs_path,
+            overwrite=overwrite,
+            user=user,
+            symlink=False,
+            verbose=0,
+        )
+    elif version_info >= (2, 0, 0, ''):
+        # Leave out the user argument with IPython 2.x series
+        install_nbextension(
+            qgridjs_path,
+            overwrite=overwrite,
+            symlink=False,
+            verbose=0,
+        )
+    else:
+        raise NotImplementedError("Only supported for IPython>=2.0.")
