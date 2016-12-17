1d7d1c6af40eeb691a84ab287d1f97e7a1f7bfe2
gondzo <klox>
2016-12-17 02:38:07 +0100

    fix multiple require configuration - fixes usage with jupyterhub

--- qgrid/qgridjs/qgrid.widget.js
+++ qgrid/qgridjs/qgrid.widget.js
@@ -25,38 +25,40 @@ define([path], function(widget) {
                 ]);
             }
 
-            var path_dictionary = {
-                jquery_drag: cdn_base_url + "/lib/jquery.event.drag-2.2",
-                slick_core: cdn_base_url + "/lib/slick.core.2.2",
-                slick_data_view: cdn_base_url + "/lib/slick.dataview.2.2",
-                slick_check_box_column: cdn_base_url + "/lib/slick.checkboxselectcolumn",
-                slick_row_selection_model: cdn_base_url + "/lib/slick.rowselectionmodel",
-                slick_grid: cdn_base_url + "/lib/slick.grid.2.2",
-                data_grid: cdn_base_url + "/qgrid",
-                date_filter: cdn_base_url + "/qgrid.datefilter",
-                text_filter: cdn_base_url + "/qgrid.textfilter",
-                slider_filter: cdn_base_url + "/qgrid.sliderfilter",
-                filter_base:  cdn_base_url + "/qgrid.filterbase",
-                editors: cdn_base_url + "/qgrid.editors",
-                handlebars: "https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0/handlebars.min"
-            };
-
             var existing_config = require.s.contexts._.config;
-            if (!existing_config.paths['underscore']){
-                path_dictionary['underscore'] = "https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min";
-            }
+            if (!existing_config.paths.data_grid){
+                var path_dictionary = {
+                    jquery_drag: cdn_base_url + "/lib/jquery.event.drag-2.2",
+                    slick_core: cdn_base_url + "/lib/slick.core.2.2",
+                    slick_data_view: cdn_base_url + "/lib/slick.dataview.2.2",
+                    slick_check_box_column: cdn_base_url + "/lib/slick.checkboxselectcolumn",
+                    slick_row_selection_model: cdn_base_url + "/lib/slick.rowselectionmodel",
+                    slick_grid: cdn_base_url + "/lib/slick.grid.2.2",
+                    data_grid: cdn_base_url + "/qgrid",
+                    date_filter: cdn_base_url + "/qgrid.datefilter",
+                    text_filter: cdn_base_url + "/qgrid.textfilter",
+                    slider_filter: cdn_base_url + "/qgrid.sliderfilter",
+                    filter_base:  cdn_base_url + "/qgrid.filterbase",
+                    editors: cdn_base_url + "/qgrid.editors",
+                    handlebars: "https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0/handlebars.min"
+                };
 
-            if (!existing_config.paths['moment']){
-                path_dictionary['moment'] = "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.3/moment.min";
-            }
+                if (!existing_config.paths['underscore']){
+                    path_dictionary['underscore'] = "https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min";
+                }
 
-            if (!existing_config.paths['jqueryui']){
-                path_dictionary['jqueryui'] = "https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min";
-            }
+                if (!existing_config.paths['moment']){
+                    path_dictionary['moment'] = "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.3/moment.min";
+                }
 
-            require.config({
-                paths: path_dictionary
-            });
+                if (!existing_config.paths['jqueryui']){
+                    path_dictionary['jqueryui'] = "https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min";
+                }
+
+                require.config({
+                    paths: path_dictionary
+                });
+            }
 
             if (typeof jQuery === 'function') {
                 define('jquery', function() { return jQuery; });
