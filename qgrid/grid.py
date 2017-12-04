acdbf518a4338a96d408a4ea460d295670a1aa37
Tim Shawver <tshawver@quantopian.com>
2017-12-04 01:23:04 -0500

    Bump the version to beta 9

--- qgrid/grid.py
+++ qgrid/grid.py
@@ -301,8 +301,8 @@ class QgridWidget(widgets.DOMWidget):
     _model_name = Unicode('QgridModel').tag(sync=True)
     _view_module = Unicode('qgrid').tag(sync=True)
     _model_module = Unicode('qgrid').tag(sync=True)
-    _view_module_version = Unicode('1.0.0-beta.8').tag(sync=True)
-    _model_module_version = Unicode('1.0.0-beta.8').tag(sync=True)
+    _view_module_version = Unicode('1.0.0-beta.9').tag(sync=True)
+    _model_module_version = Unicode('1.0.0-beta.9').tag(sync=True)
 
     _df = Instance(pd.DataFrame)
     _df_json = Unicode('', sync=True)
