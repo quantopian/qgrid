877b420d3bd83297bbcc97202b914001a85afff2
Erik Sundell <erik.i.sundell@gmail.com>
2020-04-07 20:56:49 +0200

    Align version requirements (#304)
    In #301 I forgot to bump this to 1.1.3, which probably is a harmless
    mistake, but it can be good for anyone in the future to have an easier
    time making the next release to have this align with the other versions.

--- js/src/qgrid.widget.js
+++ js/src/qgrid.widget.js
@@ -36,8 +36,8 @@ class QgridModel extends widgets.DOMWidgetModel {
       _view_name : 'QgridView',
       _model_module : 'qgrid',
       _view_module : 'qgrid',
-      _model_module_version : '^1.1.2',
-      _view_module_version : '^1.1.2',
+      _model_module_version : '^1.1.3',
+      _view_module_version : '^1.1.3',
       _df_json: '',
       _columns: {}
     });
