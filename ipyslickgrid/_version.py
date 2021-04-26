0083bee320bf8f3e2c3739f310a68a49b826c3db
tkrabel <tobiaskrabel@gmx.de>
2021-04-26 15:51:18 +0200

    0.0.2a0

--- ipyslickgrid/_version.py
+++ ipyslickgrid/_version.py
@@ -2,7 +2,7 @@
 
 # - set version_info = (0, 0, 1, "final")
 
-version_info = (0, 0, 1, "final")
+version_info = (0, 0, 2, "alpha")
 
 _specifier_ = {"alpha": "a", "beta": "b", "candidate": "rc", "final": ""}
 
@@ -10,7 +10,5 @@ __version__ = "%s.%s.%s%s" % (
     version_info[0],
     version_info[1],
     version_info[2],
-    ""
-    if version_info[3] == "final"
-    else _specifier_[version_info[3]] + str(version_info[4]),
+    _specifier_[version_info[3]],
 )
